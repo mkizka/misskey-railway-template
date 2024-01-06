2917f13ed89e31fd6b93b9b2c31dd02d0e96df5b
mkizka <g.mkizka@gmail.com>
2024-01-06 14:27:26 +0900

    add start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -eu
+
+ENV_TO_YAML=https://raw.githubusercontent.com/mkizka/misskey-railway-template/main/dist/index.js
+curl -s "$ENV_TO_YAML" | node | tee .config/default.yml
+pnpm run migrateandstart
